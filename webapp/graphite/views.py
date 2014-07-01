d209421ab190f694bfb7e37a34b537d0fd557b24
Jean Raby <jean@raby.sh>
2014-07-01 12:02:03 -0400

    Log stack traces to the exception log

--- webapp/graphite/views.py
+++ webapp/graphite/views.py
@@ -2,11 +2,15 @@ import traceback
 from django.conf import settings
 from django.http import HttpResponseServerError
 from django.template import Context, loader
+from graphite.logger import log
 
 
 def server_error(request, template_name='500.html'):
   template = loader.get_template(template_name)
+  stacktrace = traceback.format_exc()
   context = Context({
-    'stacktrace' : traceback.format_exc()
+    'stacktrace' : stacktrace
   })
+  msg = "Error handling request: %s: %s" % (request.META['REQUEST_URI'], stacktrace)
+  log.exception(msg)
   return HttpResponseServerError( template.render(context) )
